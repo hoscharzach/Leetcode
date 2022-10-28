8580b9b5f4eeffbdbc31525227a9162487e1524b
Zach Hoschar <56608122+hoscharzach@users.noreply.github.com>
2022-10-28 13:07:42 -0500

    Time: 491 ms (20.82%), Space: 50.2 MB (15.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,36 @@
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-var k = 0;
-    var maxLength = 0;
-    for(i = 0; i < s.length; i++) {
-        for (j = k; j < i; j++) {
-            if (s[i] === s[j]) {
-                k = j + 1;
-                break;
-            }
-        }
-        if (i - k + 1 > maxLength) {
-            maxLength = i - k + 1;
-        }
+  if (s === "") return 0
+  // initialize max length variable and current length
+  let max = -Infinity
+  let start = 0
+  let current = 1
+  // will have to keep one pointer on a letter while moving a second
+  // to follow a subs
+  let end = 1
+
+  // if the subs ends, set highest to max of current and max, reset
+  // current variable and end pointer
+  while (s[start]) {
+    let set = new Set()
+    // if the next letter is different, increment current and move end up
+    while (s[end] !== undefined && s[start] !== s[end]) {
+      if (!set.has(s[end])) {
+        set.add(s[end])
+        current++
+        end++
+        max = Math.max(current, max)
+      } else break
     }
-    return maxLength;
+    // else if they're the same, move start up and put end to start + 1
+    // reset current and check length of current vs max then reset current
+    start++
+    end = start + 1
+    max = Math.max(current, max)
+    current = 1
+
+  }
+
+  return max
 }
