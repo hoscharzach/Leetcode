d61996d0a4c97e03c19c3c50c18b926f6c4660aa
Zach Hoschar <56608122+hoscharzach@users.noreply.github.com>
2022-10-28 13:05:51 -0500

    Time: 99 ms (98.81%), Space: 55.5 MB (36.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,26 +2,31 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 var productExceptSelf = function(nums) {
-    let left = Array(nums.length);
-    let right = Array(nums.length);
-    let result = Array(nums.length);
-    
-    let product = 1;
-    left[0] = 1;
-    for(let i = 1; i < nums.length; i++) {
-        product *= nums[i-1];
-        left[i] = product;
-    }
-    
-    product = 1;    
-    right[nums.length - 1] = 1;
-    result[nums.length - 1] = left[nums.length - 1];
-    for(let i = nums.length - 2; i >= 0; i--) {
-        product *= nums[i+1];
-        right[i] = product;
-        result[i] = left[i] * right[i];
-    }
-    
-    return result;
+  // keep track of everything on the left of the current index
+  let left = []
+  let leftTotal = 1
+
+  // multiply everything to left of index
+  for (let i = 0; i < nums.length; i++) {
+    left[i] = leftTotal
+    leftTotal *= nums[i]
+  }
+
+  // for [1, 2, 3, 4], this gives
+  // [1, 1, 2, 6] so the 1 needs to be multiplied by every number except 1
+  // which would happen if we multiplied every number backwards to the 1
+
+  let rightTotal = 1
+  // do another loop adding up the product the other way
+  for (let i = nums.length - 1; i >= 0; i--) {
+    left[i] *= rightTotal
+    rightTotal *= nums[i]
+  }
+
+  return left
 };
