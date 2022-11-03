fe5b2c91291fe950da4b12ef254bc8c2065e6e31
Zach Hoschar <56608122+hoscharzach@users.noreply.github.com>
2022-11-03 14:18:11 -0500

    Time: 7 ms (67.48%), Space: 3.9 MB (49.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.go
+++ 0200-number-of-islands/0200-number-of-islands.go
@@ -15,17 +15,22 @@ func numIslands(grid [][]byte) int {
 
 func markLand(grid [][]byte, i int, j int) {
     
+    // make new slice of directions
     a := [][]int{
         {0, 1}, {1, 0}, {-1, 0}, {0, -1},
     }
     
+    // conditionals for checking if the node is a valid node
     if i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0]) && string(grid[i][j]) == "1" {
-        // fmt.Println(grid[i][j])
+        
+        // set the node to byte "2"
         grid[i][j] = 50
+        // iterate through directions and recursively call markLand on each direction
         for _, y := range(a) {
         markLand(grid, i + y[0], j + y[1])
         }
     } else {
+        // if it's not a valid node, then just return
         return
     } 
     
