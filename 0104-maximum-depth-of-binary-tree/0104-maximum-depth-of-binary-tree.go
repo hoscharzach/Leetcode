d084810e46040dc2481101e76552d7dd8e46d537
Zach Hoschar <56608122+hoscharzach@users.noreply.github.com>
2022-11-07 10:33:24 -0600

    Time: 9 ms (32.67%), Space: 4.4 MB (12.53%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
@@ -11,12 +11,14 @@ func maxDepth(root *TreeNode) int {
         return 0
     }
     
-    x := 1 + maxDepth(root.Left)
-    y := 1 + maxDepth(root.Right)
+    return int(math.Max(float64(1 + maxDepth(root.Left)), float64(1 + maxDepth(root.Right))))
     
-    if x > y {
-        return x
-    } else {
-        return y
-    }
+//     x := 1 + maxDepth(root.Left)
+//     y := 1 + maxDepth(root.Right)
+    
+//     if x > y {
+//         return x
+//     } else {
+//         return y
+//     }
 }
